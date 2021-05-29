fa0f504063346d677d8c9ac61bfab5f8dbf07042
iluzych <ieva.daugvilaite@gmail.com>
2021-05-29 11:15:30 +0300

    Gravity Flip

--- js/gravityFlip.js
+++ js/gravityFlip.js
@@ -2,3 +2,6 @@ const flip=(d, a)=>{
   if(d === 'R') return a.sort((a, b) => a - b);
   if(d === 'L') return a.sort((a, b) => b - a);
 }
+
+console.log(flip('R', [3, 2, 1, 2]), [1, 2, 2, 3]);
+console.log(flip('L', [1, 4, 5, 3, 5]), [5, 5, 4, 3, 1]);
